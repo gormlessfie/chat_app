4dd055694e4ad73c7bd8437d62bb34db18a612f3
gormlessfie <gmlessfie@gmail.com>
2022-01-17 14:10:16 -0800

    Configure devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true,
+                       length: { in: 3..12 },
+                       format: { with: /\A[a-zA-Z0-9]*\Z/, message: 'only allow letters and numbers'}
+end
